07ab0693cf64962eaa867c6e3a31d26631b6b8f4
kebabmaneater <laurimikael8@gmail.com>
2025-12-01 16:55:21 +0200

    updated config by rebuilding it

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -30,7 +30,7 @@ require("lazy").setup({
                 version = false, -- always use the latest git commit
                 -- version = "*", -- try installing the latest stable version for plugins that support semver
         },
-        install = {},
+        install = { colorscheme = { "tokyonight", "habamax" } },
         checker = {
                 enabled = true, -- check for plugin updates periodically
                 notify = false, -- notify on update
@@ -45,13 +45,9 @@ require("lazy").setup({
                                 -- "netrwPlugin",
                                 "tarPlugin",
                                 "tohtml",
-                                -- "tutor",
+                                "tutor",
                                 "zipPlugin",
                         },
                 },
         },
-})
-
-require("lspconfig").qmlls.setup({
-        cmd = { "qmlls", "-E" },
 })
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,4 +2,12 @@ return {
         -- add gruvbox
         { "sainnhe/gruvbox-material" },
         { "https://github.com/sam4llis/nvim-tundra" },
+        { "rebelot/kanagawa.nvim" },
+
+        {
+                "LazyVim/LazyVim",
+                opts = {
+                        colorscheme = "kanagawa-dragon",
+                },
+        },
 }
